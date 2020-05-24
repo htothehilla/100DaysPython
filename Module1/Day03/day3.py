1c6f82ffe6551bf093d8b4c99845193583deb0fc
hilla <47607949+htothehilla@users.noreply.github.com>
2020-05-24 17:00:06 +0100

    Create day3.py

---
+++ Module1/Day03/day3.py
@@ -0,0 +1,36 @@
+git@gist.github.com:87c7d599fe577b9c2e8490048e7a37b1.git
+
+list = [10,6]
+
+print(list[0] + list[1])
+print(list[0] - list[1])
+print(list[0] * list[1])
+print(list[0] / list[1])
+print(list[0] // list[1])
+print(list[0] % list[1])
+print(list[0] ** list[1])
+
+print(8*(2+2)+3**2+1)
+print(10//6.0)
+
+print("python"+": It's a fun language to learn")
+print("\"this is a qoute.\"")
+print("c:\\newfolder")
+#RELATIONAL OPS
+
+list_2 =[1,2]
+print(list_2[0] == list_2[0])
+print(list_2[0] ==list_2[1])
+
+print(list_2[0] != list_2[0])
+print(list_2[0] !=list_2[1])
+
+print(list_2[0] > list_2[0])
+print(list_2[0] >list_2[1])
+
+print(list_2[0] >= list_2[0])
+print(list_2[0] >=list_2[1])
+
+print(True and False)
+print(True or False)
+print(not False)
