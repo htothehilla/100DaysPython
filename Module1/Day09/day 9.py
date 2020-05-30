a6ec3fbd4d722edd08aa5326c1d88de87fd238d9
hilla <47607949+htothehilla@users.noreply.github.com>
2020-05-30 10:05:24 +0100

    Add files via upload

---
+++ Module1/Day09/day 9.py
@@ -0,0 +1,14 @@
+wayne = "Toughest Guy in Letterkenny"
+wayne[::1]
+
+text = "That's a Texas sized 10-4."[0:9:2]
+print(text)
+
+quotes = ["Pitter patter, let's get at 'er", "Hard no!", "H'are ya now?", "Good-n-you?", "Not so bad.", "Is that what you appreciates about me?"]
+print(quotes[0])
+print(quotes[3:])
+print(quotes[:3])
+print(quotes[::3])
+
+exchange = quotes[2:5]
+print(exchange)
