e01345283bbbef65d30e30933cb01e3b9c60c1f4
hillarykjuma <hillarykimj@gmail.com>
2020-06-12 15:34:13 +0100

    day18 packages

--- Module2/Day18/day18.py
+++ Module2/Day18/day18.py
@@ -29,3 +29,31 @@ turtle.shape("turtle")
 for x in range(100):
     turtle.forward(x)
     turtle.left(angle)
+#web
+import webbrowser
+webbrowser.open("https://docs.python.org/3/library/webbrowser.html")
+chrome = webbrowser.get(using='google-chrome')
+chrome.open_new("https://www.youtube.com/watch?v=CMNry4PE93Y")
+help(webbrowser)
+#time package (execution of code)
+import time
+start = time.time()
+print("Program Start")
+time.sleep(5)
+print("Program End. The total execution time was {} sec.".format(time.time() - start))
+#datetime
+from datetime import datetime
+print(datetime.today())
+print("Day: {}\nMonth: {}\nYear: {}".format(datetime.today().day, datetime.today().month, datetime.today().year))
+if datetime.today() > datetime(datetime.today().year, 7, 4):
+    days_until = (datetime(datetime.today().year + 1, 9, 4) - datetime.today()).days
+    age = (datetime.today().year + 1) - 1998
+else:
+    days_until = (datetime(datetime.today().year, 9, 4) - datetime.today()).days
+    age = datetime.today().year - 1998
+print("There are {} days left until My birthday. I will be {} years old.".format(days_until, age))
+# OS Pacakge
+import os
+print(os.getcwd())
+print(os.cpu_count())
+print(os.getlogin())
