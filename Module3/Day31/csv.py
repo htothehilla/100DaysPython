ac7c3977d0e807eb23043851d7323f6389ef4f39
hillarykjuma <hillarykimj@gmail.com>
2020-08-17 20:08:15 +0100

    csv mainupluation

--- Module3/Day31/csv.py
+++ Module3/Day31/csv.py
@@ -23,3 +23,45 @@ try:
 finally:
     mash.close()
 
+# it's important to log things to see how long they take so you know when it's finished
+
+#spacing
+mash = open("mash.csv", "wt")
+try:
+    writer = csv.writer(mash, lineterminator="\n")
+    writer.writerow(("actor", "character", "episodes"))
+    for i in range(len(actor)):
+        writer.writerow((actor[i], character[i], episodes[i]))
+finally:
+    mash.close()
+
+#adding data
+mash = open("mash.csv", "at")
+try:
+    writer = csv.writer(mash, lineterminator="\n")
+    writer.writerow(("This includes a, comma", "How will that affect, it?", "This normally contains integers"))
+finally:
+    mash.close()
+
+# add a deliminiter
+mash = open("mash_pipe.csv", "wt")
+try:
+    writer = csv.writer(mash, delimiter="|", lineterminator="\n")
+    writer.writerow(("actor", "character", "episodes"))
+    for i in range(len(actor)):
+        writer.writerow((actor[i], character[i], episodes[i]))
+    writer.writerow(("This includes a, comma", "How will that affect, it?", "This normally contains integers"))
+finally:
+    mash.close()
+
+mash = open("mash.tsv", "wt")
+try:
+    writer = csv.writer(mash, delimiter="\t", lineterminator="\n")
+    writer.writerow(("actor", "character", "episodes"))
+    for i in range(len(actor)):
+        writer.writerow((actor[i], character[i], episodes[i]))
+    writer.writerow(("This includes a, comma", "How will that affect, it?", "This normally contains integers"))
+finally:
+    mash.close()
+
+
